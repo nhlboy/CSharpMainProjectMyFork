77896f0047ad056fbeb6c1667a0b4fbccbf74cfe
nhlboy <48943178+nhlboy@users.noreply.github.com>
2023-12-31 02:20:39 -0500

    Implimentation of localHeight formula

--- Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
+++ Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
@@ -29,7 +29,10 @@ namespace Model.Runtime.Projectiles
             ///////////////////////////////////////
             // Insert you code here
             ///////////////////////////////////////
-
+            
+            float maxHeight = totalDistance * 0.6f;
+            localHeight = maxHeight * (-1*(t * 2 - 1) * (t * 2 - 1) + 1);
+            Debug.Log(localHeight);
 
             ///////////////////////////////////////
             // End of the code to insert
