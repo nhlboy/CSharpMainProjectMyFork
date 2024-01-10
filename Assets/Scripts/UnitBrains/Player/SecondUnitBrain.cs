fb557e27225639b6a3493409182a9a5f25038f86
nhlboy <k.ivashchenko@gmail.com>
2024-01-09 23:26:11 -0500

    Shooting modification of a specific unit.
    Homework #3

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Model.Runtime.Projectiles;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace UnitBrains.Player
@@ -12,17 +14,60 @@ namespace UnitBrains.Player
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
         private bool _overheated;
-        
+
+
+        ///////////////////////////////////////
+        // Homework 1.3 (1st block, 3rd module)
+        ///////////////////////////////////////   
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
+
+
             float overheatTemperature = OverheatTemperature;
-            ///////////////////////////////////////
-            // Homework 1.3 (1st block, 3rd module)
-            ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
+
+            // Реализуй механику нагрева оружия.
+            // a.Тебе необходимо проверить, а не перегрелось ли уже оружие.
+            // Для этого получи текущую температуру с помощью метода GetTemperature,
+            // который уже реализован в данном скрипте. И если текущая температура,
+            // возвращенная этим методом больше или равна температуре перегрева - overheatTemperature,
+            // то выполнение метода следует прервать, до остывания оружия,
+            // которое происходит автоматически.
+
+            float currentTemperature = GetTemperature();
+
+            while (true)
+            {
+                if (currentTemperature >= overheatTemperature)
+                {
+                    break;
+                }
+
+                // Реализуй механику увеличения снарядов с каждым выстрелом.
+                // a.Обрати внимание на код в методе GenerateProjectiles, который
+                // генерирует снаряды и добавляет их в некий лист.Тебе необходимо
+                // обернуть его в цикл так, чтобы при каждом выстреле количество
+                // снарядов соответствовало текущей температуре оружия.
+                // b.Какой цикл для этого больше подходит - реши самостоятельно.
+                else
+                {
+                    for (int i = 0; i < overheatTemperature; i++)
+                    {
+                        var projectile = CreateProjectile(forTarget);
+                        AddProjectileToList(projectile, intoList);
+
+                        // b.Каждый вызов метода GenerateProjectiles соответствует одному выстрелу.
+                        // С каждым выстрелом нагрев оружия должен увеличиваться.Для этого используй
+                        // метод IncreaseTemperature, он также уже реализован в данном скрипте, тебе
+                        // необходимо лишь его вызвать.
+
+                        IncreaseTemperature();
+                    }
+                }
+            }
+
             ///////////////////////////////////////
         }
+
 
         public override Vector2Int GetNextStep()
         {
@@ -43,12 +88,14 @@ namespace UnitBrains.Player
             ///////////////////////////////////////
         }
 
+
+
         public override void Update(float deltaTime, float time)
         {
             if (_overheated)
-            {              
+            {
                 _cooldownTime += Time.deltaTime;
-                float t = _cooldownTime / (OverheatCooldown/10);
+                float t = _cooldownTime / (OverheatCooldown / 10);
                 _temperature = Mathf.Lerp(OverheatTemperature, 0, t);
                 if (t >= 1)
                 {
@@ -60,7 +107,7 @@ namespace UnitBrains.Player
 
         private int GetTemperature()
         {
-            if(_overheated) return (int) OverheatTemperature;
+            if (_overheated) return (int)OverheatTemperature;
             else return (int)_temperature;
         }
 
